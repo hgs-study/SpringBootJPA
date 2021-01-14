d6a8fcb53e08720da6a80df8a3dc1cf9d66ca70a
hgs-study <hgstudy_@naver.com>
2021-01-14 23:39:11 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/example/springboot_jpa/domain/User.java
@@ -0,0 +1,36 @@
+package com.example.springboot_jpa.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column( nullable = false)
+    private String name;
+
+    @Column( nullable = false)
+    private int age;
+
+    public User update(Long id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+        return this;
+    }
+
+}
--- src/main/java/com/example/springboot_jpa/web/PageController.java
+++ src/main/java/com/example/springboot_jpa/web/PageController.java
@@ -1,6 +1,8 @@
 package com.example.springboot_jpa.web;
 
+import com.example.springboot_jpa.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +12,16 @@ public class PageController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+    @GetMapping("/loginPage")
+    public String login(Model model){
+
+        User user = User.builder()
+                    .name("현건수")
+                    .age(13)
+                    .build();
+        model.addAttribute("User",user);
+
+        return "login";
     }
 }
